d32723a678192726b49ab5b20c70910f035bbf07
Alex Wang <wxb_nudt@163.com>
2018-11-20 11:28:56 +0800

    revise EmpBasic component.

--- src/components/emp/EmpBasic.vue
+++ src/components/emp/EmpBasic.vue
@@ -80,7 +80,7 @@
                     <el-radio style="margin-left: 15px" label="实习生">实习生</el-radio>
                   </el-radio-group>
                 </el-col>
-               </el-row>
+              </el-row>
               <el-row style="margin-top: 10px">
                 <el-col :span="5">
                   所属部门:
@@ -120,24 +120,64 @@
             </div>
           </transition>
           <!--用来展示员工信息的表格-->
-          <el-table
-            :data="emps"
-            style="width: 100%">
-            <el-table-column
-              prop="id"
-              label="id"
-              width="180">
+          <el-table :data="emps" size="mini" stripe border style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="35" fixed/>
+            <el-table-column prop="name" label="姓名" width="100" fixed/>
+            <el-table-column prop="gender" label="性别" width="50"/>
+            <el-table-column prop="birthday" label="生日" width="100">
+              <!--日期类型要转换后显示-->
+              <template slot-scope="scope">{{ scope.row.birthday | formatDate}}</template>
             </el-table-column>
-            <el-table-column
-              prop="name"
-              label="姓名"
-              width="180">
+            <el-table-column prop="idCard" width="170" align="left" label="身份证号码"/>
+            <el-table-column prop="wedlock" width="60" align="left" label="婚姻状况"/>
+            <!--民族是读取nation的name属性，这个东西在EmployeeMapper.xml中定义-->
+            <el-table-column prop="nation.name" width="60" align="left" label="民族"/>
+            <el-table-column prop="nativePlace" width="80" align="left" label="籍贯"/>
+            <el-table-column prop="politicsStatus.name" width="80" align="left" label="政治面貌"/>
+            <el-table-column prop="email" width="160" align="left" label="邮箱"/>
+            <el-table-column prop="phone" width="110" align="left" label="电话号码"/>
+            <el-table-column prop="address" width="170" align="left" label="联系地址"/>
+            <el-table-column prop="department.name" width="80" align="left" label="所属部门"/>
+            <el-table-column prop="jobLevel.name" width="100" align="left" label="职称"/>
+            <el-table-column prop="position.name" width="100" align="left" label="职务"/>
+            <el-table-column prop="engageForm" width="90" align="left" label="聘用形式"/>
+            <el-table-column prop="tiptopDegree" width="80" align="left" label="最高学历"/>
+            <el-table-column prop="specialty" width="120" align="left" label="所属专业"/>
+            <el-table-column prop="school" width="140" align="left" label="毕业院校"/>
+            <el-table-column prop="beginDate" width="100" align="left" label="入职日期">
+              <template slot-scope="scope">{{ scope.row.beginDate | formatDate}}</template>
             </el-table-column>
-            <el-table-column
-              prop="gender"
-              label="性别">
+            <el-table-column prop="workState" width="80" align="left" label="在职状态"/>
+            <el-table-column prop="workID" width="100" align="left" label="工号"/>
+            <el-table-column prop="contractTerm" width="80" align="left" label="合同期限"/>
+            <el-table-column prop="conversionTime" width="100" align="left" label="转正日期">
+              <template slot-scope="scope">{{ scope.row.beginDate | formatDate}}</template>
+            </el-table-column>
+            <el-table-column prop="notWorkDate" width="100" align="left" label="离职日期">
+              <template slot-scope="scope">{{ scope.row.beginDate | formatDate}}</template>
+            </el-table-column>
+            <el-table-column prop="beginContract" width="100" align="left" label="合同起始日期">
+              <template slot-scope="scope">{{ scope.row.beginDate | formatDate}}</template>
+            </el-table-column>
+            <el-table-column prop="endContract" width="100" align="left" label="合同终止日期">
+              <template slot-scope="scope">{{ scope.row.beginDate | formatDate}}</template>
+            </el-table-column>
+            <el-table-column label="操作" width="200" fixed="right">
+              <template slot-scope="scope">
+                <el-button size="mini" type="info" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+              </template>
             </el-table-column>
           </el-table>
+          <div style="display: flex;justify-content: space-between;margin: 2px">
+            <el-button type="danger" size="mini">批量删除</el-button>
+            <el-pagination
+              @current-change="handleCurrentChange"
+              :page-size="10"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="400">
+            </el-pagination>
+          </div>
         </div>
       </el-main>
     </el-container>
@@ -158,6 +198,7 @@
         deps: [],
         keywords: '', // 搜索关键字
         currentPage: 1, // 当前页
+        multipleSelection: [], // 当前选中行
         advanceSearchViewVisible: false, // 高级搜索对话框是否显示
         faangledoubleup: 'fa-angle-double-up', // 图标
         faangledoubledown: 'fa-angle-double-down', // 图标
@@ -210,7 +251,7 @@
       this.loadEmps()
     },
     methods: {
-      initData() { // 读取字典表
+      initData () { // 读取字典表
         // load dictionary data
         var _this = this
         this.getRequest('/employee/basic/basicdata').then(resp => {
@@ -222,43 +263,43 @@
             _this.joblevels = data.joblevels
             _this.deps = data.deps
           }
-        });
+        })
       },
       loadEmps () { // 读取员工表
         var _this = this
         this.getRequest('/employee/basic/emp?page=' + this.currentPage + '&size=10')
           .then(resp => {
-          if (resp && resp.status === 200) {
-            var data = resp.data;
-            _this.emps = data.emps;
-            console.log('load ' + data.count + ' employees!')
-          }
-        });
+            if (resp && resp.status === 200) {
+              var data = resp.data
+              _this.emps = data.emps
+              console.log('load ' + data.count + ' employees!')
+            }
+          })
       },
-      keywordsChange() {
+      keywordsChange () {
         this.$message({message: 'keywordsChange()', type: 'success'})
       },
-      searchEmp() {
+      searchEmp () {
         this.$message({message: 'searchEmp()', type: 'success'})
       },
-      showAdvanceSearchView() {
-        this.advanceSearchViewVisible = !this.advanceSearchViewVisible;
+      showAdvanceSearchView () {
+        this.advanceSearchViewVisible = !this.advanceSearchViewVisible
       },
-      fileUploadSuccess() {
+      fileUploadSuccess () {
         this.$message({message: '文件上传成功', type: 'success'})
       },
-      fileUploadError() {
-        this.$message.error('文件上传失败');
+      fileUploadError () {
+        this.$message.error('文件上传失败')
       },
-      beforeFileUpload() {
+      beforeFileUpload () {
         this.$message({message: '文件上传beforeFileUpload', type: 'success'})
       },
       // 添加员工
-      showAddEmpView() {
+      showAddEmpView () {
         this.$message({message: '添加员工', type: 'success'})
       },
       // 导出数据
-      exportEmps() {
+      exportEmps () {
         this.$message({message: '导出数据', type: 'success'})
       },
       // 点击部门tree控件节点的响应
@@ -279,6 +320,17 @@
       },
       cancelSearch () {
         this.$message({message: 'cancelSearch', type: 'success'})
+      },
+      handleSelectionChange (val) {
+        this.$message({message: 'handleSelectionChange列表选中', type: 'success'})
+        this.multipleSelection = val
+      },
+      // 操作表中的行
+      handleEdit (index, row) {
+        console.log(index, row)
+      },
+      handleDelete (index, row) {
+        console.log(index, row)
       }
     }
   }
