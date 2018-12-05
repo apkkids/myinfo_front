03d2c724e7553b3794133e451ee7fed7c0742a07
apkkids <apkkids@163.com>
2018-12-05 20:12:36 +0800

    add SysHr component.

--- src/components/system/SysHr.vue
+++ src/components/system/SysHr.vue
@@ -2,42 +2,20 @@
   <div style="margin-top: 10px">
     <div style="margin-bottom: 10px; display:flex;justify-content: space-between; align-items: center ">
       <div style="display:inline">
-      <el-input
-        placeholder="默认展示部分用户，请输入关键字搜索更多用户..."
-        style="width:400px; margin-right: 10px"
-        size="small"
-        prefix-icon="el-icon-search"
-        v-model="keywords">
-      </el-input>
-      <el-button size="small" type="primary" @click="searchHr">搜索</el-button>
+        <el-input
+          placeholder="默认展示部分用户，请输入关键字搜索更多用户..."
+          style="width:400px; margin-right: 10px"
+          size="small"
+          prefix-icon="el-icon-search"
+          v-model="keywords">
+        </el-input>
+        <el-button size="small" type="primary" @click="searchHr">搜索</el-button>
       </div>
       <div style="display:inline;">
-      <el-button size="small" type="primary" icon="el-icon-plus" @click="dialogFormVisible = true"
-                 style="margin-right: 10px;">注册管理员</el-button>
+        <el-button size="small" type="primary" icon="el-icon-plus" @click="dialogFormVisible = true"
+                   style="margin-right: 10px;">注册管理员
+        </el-button>
       </div>
-    </div>
-    <!--此处的div就是放了一个对话框-->
-    <div>
-      <el-dialog style="color:#409EFF" title="注册管理员" :visible.sync="dialogFormVisible">
-        <el-row :gutter="20">
-          <el-col :span="6"><div>
-            <el-button>text</el-button>
-          </div></el-col>
-          <el-col :span="6"><div>
-            <el-button>text</el-button>
-          </div></el-col>
-          <el-col :span="6"><div>
-            <el-button>text</el-button>
-          </div></el-col>
-          <el-col :span="6"><div>
-            <el-button>text</el-button>
-          </div></el-col>
-        </el-row>
-        <div slot="footer" >
-          <el-button size="mini" @click="dialogFormVisible = false">取 消</el-button>
-          <el-button size="mini" type="primary" @click="dialogFormVisible = false">确 定</el-button>
-        </div>
-      </el-dialog>
     </div>
     <div style="display: flex;justify-content: space-around;flex-wrap: wrap;text-align: left">
       <el-card style="width:350px; margin-bottom: 10px" v-for="(item,index) in sysusers" :key="item.id"
@@ -86,6 +64,100 @@
         </div>
       </el-card>
     </div>
+    <!--此处的div就是放了一个注册对话框-->
+    <el-form size="mini" :rules="rules" :model="user" ref="addSysUserForm" style="margin: 0px;padding: 0px;" label-width="100px">
+      <div style="text-align: left">
+        <el-dialog style="color:#409EFF" title="注册管理员" :visible.sync="dialogFormVisible">
+          <el-row :gutter="20">
+            <el-col :span="6">
+              <div>
+                <el-form-item label="姓名:" prop="name">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入姓名" v-model="user.name" size="mini"
+                            style="width: 140px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div>
+                <el-form-item label="手机:" prop="phone">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入手机" v-model="user.phone" size="mini"
+                            style="width: 140px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+            <el-col :span="5">
+              <div>
+                <el-form-item label="电话:" prop="telephone">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入电话" v-model="user.telephone" size="mini"
+                            style="width: 150px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+            <el-col :span="7">
+              <div>
+                <el-form-item label="状态:" prop="enabled">
+                  <el-radio-group v-model="user.enabled">
+                    <el-radio :label="1">启用</el-radio>
+                    <el-radio :label="0" style="margin-left: 15px">禁用</el-radio>
+                  </el-radio-group>
+                </el-form-item>
+              </div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="6">
+              <div>
+                <el-form-item label="登录名:" prop="username">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入登录名" v-model="user.username" size="mini"
+                            style="width: 140px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div>
+                <el-form-item label="密码:" prop="password">
+                  <el-input type="password" prefix-icon="el-icon-edit" placeholder="输入密码" v-model="user.password"
+                            size="mini"
+                            style="width: 140px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div>
+                <el-form-item label="图片地址:" prop="userface">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入图片地址" v-model="user.userface" size="mini"
+                            style="width: 300px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="24">
+              <div>
+                <el-form-item label="地址:" prop="address">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入地址" v-model="user.address" size="mini"
+                            style="width: 600px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="24">
+              <div>
+                <el-form-item label="备注:" prop="remark">
+                  <el-input prefix-icon="el-icon-edit" placeholder="输入备注" v-model="user.remark" size="mini"
+                            style="width: 600px"/>
+                </el-form-item>
+              </div>
+            </el-col>
+          </el-row>
+          <div slot="footer">
+            <el-button size="mini" @click="resetForm('addSysUserForm')">取 消</el-button>
+            <el-button size="mini" type="primary" @click="submitForm('addSysUserForm')">确 定</el-button>
+          </div>
+        </el-dialog>
+      </div>
+    </el-form>
   </div>
 </template>
 
@@ -102,39 +174,76 @@
         rolesChanged: false, // 选中的角色是否改变
         cardloading: [], // card是否正在loading
         allRoles: [], // 系统中所有角色
-        sysusers: [] // 存储所有管理员变量的数组
+        user: {// 注册时的当前用户
+          name: '',
+          phone: '',
+          telephone: '',
+          address: '',
+          enabled: 1,
+          username: '',
+          password: '',
+          userface: '',
+          remark: ''
+        },
+        sysusers: [], // 存储所有管理员变量的数组
+        // form验证规则
+        rules: {
+          name: [{required: true, message: '请输入管理员名称', trigger: 'blur'}],
+          address: [{required: true, message: '请输入地址', trigger: 'blur'}],
+          phone: [{required: true, message: '请输入电话', trigger: 'blur'}],
+          telephone: [{required: true, message: '请输入手机', trigger: 'blur'}],
+          enabled: [{required: true, message: '请选择用户状态', trigger: 'blur'}],
+          username: [{required: true, message: '请输入用户登录名', trigger: 'blur'}],
+          password: [{required: true, message: '请输入密码', trigger: 'blur'},
+            {min: 6, max: 100, message: '密码长度不能小于6', trigger: 'blur'}]
+        }
       }
     },
     mounted: function () {
-      this.loadAllRoles();
-      this.loadSysUsers();
+      this.loadAllRoles()
+      this.loadSysUsers()
     },
     methods: {
-      // 加载系统中所有角色
-      loadAllRoles () {
-        var _this = this;
-        this.getRequest('/system/basic/roles').then(resp => {
-          if (resp && resp.status === 200) {
-            _this.allRoles = resp.data;
+      submitForm (formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+            // this.dialogFormVisible = false;
+          } else {
+            console.log('error submit!!');
+            return false;
           }
         });
       },
+      resetForm (formName) {
+        this.$refs[formName].resetFields();
+        // this.dialogFormVisible = false;
+      },
+      // 加载系统中所有角色
+      loadAllRoles () {
+        var _this = this
+        this.getRequest('/system/basic/roles').then(resp => {
+          if (resp && resp.status === 200) {
+            _this.allRoles = resp.data
+          }
+        })
+      },
       searchHr () {
-        this.loadSysUsers();
+        this.loadSysUsers()
       },
       // 按照关键字keywords载入多个管理员
       loadSysUsers () {
         this.$message({message: 'loadSysUsers', type: 'success'})
-        var _this = this;
-        var searchWord = this.keywords;
+        var _this = this
+        var searchWord = this.keywords
         if (searchWord === '') { // 如果关键字为空，则赋予all
-          searchWord = 'all';
+          searchWord = 'all'
         }
         this.getRequest('/system/sysuser/' + searchWord).then(resp => {
           if (resp && resp.status === 200) {
-            _this.sysusers = resp.data;
+            _this.sysusers = resp.data
             for (var i = 0; i < _this.sysusers.length; i++) {
-              _this.cardloading[i] = false;
+              _this.cardloading[i] = false
             }
           }
         })
@@ -147,41 +256,41 @@
           type: 'warning'
         }).then(() => {
           // 提交从数据库中删除的请求
-          var _this = this;
+          var _this = this
           this.deleteRequest('/system/sysuser/del/' + id).then(resp => {
             if (resp && resp.status === 200) {
-              _this.sysusers.splice(index, 1);
-              _this.cardloading.splice(index, 1);
+              _this.sysusers.splice(index, 1)
+              _this.cardloading.splice(index, 1)
             }
-          });
-          this.$message({type: 'success', message: '管理员删除成功!'});
+          })
+          this.$message({type: 'success', message: '管理员删除成功!'})
         }).catch(() => {
-          this.$message({type: 'info', message: '取消删除操作'});
-        });
+          this.$message({type: 'info', message: '取消删除操作'})
+        })
       },
       selectChange () {
-        this.rolesChanged = true;
+        this.rolesChanged = true
       },
       // 在弹出控件弹出前，准备好管理员的角色数组selRoles
       loadSelRoles (sysUserRoles) {
-        this.rolesChanged = false;
-        this.selRoles = [];
-        this.selRolesBak = [];
+        this.rolesChanged = false
+        this.selRoles = []
+        this.selRolesBak = []
         sysUserRoles.forEach(role => {
-          this.selRoles.push(role.id);
-          this.selRolesBak.push(role.id);
-        });
+          this.selRoles.push(role.id)
+          this.selRolesBak.push(role.id)
+        })
       },
       refreshUser (userid, index) {
         // this.cardloading.splice(index, 1, true);
-        this.cardloading[index] = true;
-        console.log(this.cardloading);
-        var _this = this;
+        this.cardloading[index] = true
+        console.log(this.cardloading)
+        var _this = this
         this.putRequest('/system/sysuser/id/' + userid).then(resp => {
           if (resp && resp.status === 200) {
-            _this.sysusers.splice(index, 1, resp.data);
-            _this.cardloading[index] = false;
-            console.log(this.cardloading);
+            _this.sysusers.splice(index, 1, resp.data)
+            _this.cardloading[index] = false
+            console.log(this.cardloading)
           }
         })
       },
@@ -194,23 +303,23 @@
             type: 'warning'
           }).then(() => {
             // 保存到数据库
-            var _this = this;
-            var roles = this.selRoles;
+            var _this = this
+            var roles = this.selRoles
             this.putRequest('/system/sysuser/roles', {
               currentId: currentId,
               rids: roles
             }).then(resp => {
               if (resp && resp.status === 200) {
-                var data = resp.data.msg;
+                var data = resp.data.msg
                 if (data === 'success') {
-                  _this.refreshUser(currentId, index);
+                  _this.refreshUser(currentId, index)
                 }
               }
-            });
-            this.$message({type: 'success', message: '管理员角色改变成功!'});
+            })
+            this.$message({type: 'success', message: '管理员角色改变成功!'})
           }).catch(() => {
-            this.$message({type: 'info', message: '取消角色改变'});
-          });
+            this.$message({type: 'info', message: '取消角色改变'})
+          })
         }
       }
     }
