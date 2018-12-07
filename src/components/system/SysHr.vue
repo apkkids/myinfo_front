bd4a9daa9c523080218fb8d90833f188079c2a9b
apkkids@163.com <apkkids@163.com>
2018-12-07 10:32:46 +0800

    revise SystemHr.vue.

--- src/components/system/SysHr.vue
+++ src/components/system/SysHr.vue
@@ -151,6 +151,23 @@
               </div>
             </el-col>
           </el-row>
+          <el-row :gutter="20">
+            <el-col :span="24">
+              <div>
+                <el-form-item label="用户头像:">
+                  <el-upload
+                    class="avatar-uploader"
+                    action="/system/sysuser/uploadImage"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                  </el-upload>
+                </el-form-item>
+              </div>
+            </el-col>
+          </el-row>
           <div slot="footer">
             <el-button size="mini" @click="resetForm('addSysUserForm')">取 消</el-button>
             <el-button size="mini" type="primary" @click="submitForm('addSysUserForm')">确 定</el-button>
@@ -167,6 +184,7 @@
     name: 'SysHr',
     data () {
       return {
+        imageUrl: '',
         dialogFormVisible: false,
         keywords: '',
         selRoles: [], // 当前选中的角色
@@ -328,6 +346,21 @@
             this.$message({type: 'info', message: '取消角色改变'})
           })
         }
+      },
+      handleAvatarSuccess (res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload (file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
     }
   }
@@ -337,5 +370,28 @@
   .user-info {
     font-size: 12px;
     color: #09c0f6;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 </style>
