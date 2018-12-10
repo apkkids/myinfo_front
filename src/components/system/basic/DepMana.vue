36602a9b2a6fb6060421871024038220e7c4b95b
apkkids@163.com <apkkids@163.com>
2018-12-10 16:17:33 +0800

    revise SystemHr.vue.

--- src/components/system/basic/DepMana.vue
+++ src/components/system/basic/DepMana.vue
@@ -9,16 +9,64 @@
           v-model="keywords">
         </el-input>
       </div>
-      <div>body</div>
+      <div>
+        <el-tree
+          :data="treeData"
+          :props="defaultProps"
+          show-checkbox
+          node-key="id"
+          :expand-on-click-node="false">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            type="primary"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            type="danger"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+        </el-tree>
+      </div>
     </div>
 </template>
 
 <script>
+/* eslint-disable semi */
+
 export default {
   name: 'DepMana',
   data () {
     return {
-      keywords: ''
+      keywords: '',
+      treeData: [], // 树状控件对应的数据，分层加载
+      defaultProps: {
+        label: 'name',
+        isLeaf: 'leaf',
+        children: 'children'
+      }
+    }
+  },
+  mounted: function () {
+    this.loadTreeData();
+  },
+  methods: {
+    loadTreeData () {
+      var _this = this;
+      // 读取树根节点
+      this.getRequest('/system/basic/dep/-1').then(resp => {
+        if (resp && resp.status === 200) {
+          _this.treeData = resp.data;
+          console.log(_this.treeData);
+        }
+      });
     }
   }
 }
