1fd81410007a6dd1c9fc59395c8801060700a580
apkkids@163.com <apkkids@163.com>
2018-12-12 11:27:40 +0800

    revise SystemHr.vue.

--- src/components/system/basic/PosMana.vue
+++ src/components/system/basic/PosMana.vue
@@ -1,25 +1,32 @@
 <template>
+  <div>
+    <div style="display: flex; margin-bottom: 10px ">
+      <el-input v-model="input" placeholder="请输入要添加的职位名称" prefix-icon="el-icon-plus"
+                style="width:500px;margin-right: 10px"></el-input>
+      <el-button type="primary" size="mini" icon="el-icon-plus" @click="addPosition">添加</el-button>
+    </div>
     <div>
-      <el-table
-        :data="tableData"
-        style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
+      <el-table :data="positions" v-loading="loading" size="mini" stripe border style="width: 100%"
+                @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55"/>
+        <el-table-column prop="id" label="编号" width="180"></el-table-column>
+        <el-table-column prop="name" label="职位名称" width="180"></el-table-column>
+        <el-table-column prop="createDate" label="创建时间" width="100">
+          <!--日期类型要转换后显示-->
+          <template slot-scope="scope">{{ scope.row.createDate | formatDate}}</template>
         </el-table-column>
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="地址">
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
         </el-table-column>
       </el-table>
-      <el-button type="danger" @click="getString">从后端请求一段话</el-button>
     </div>
+    <div style="display:flex;margin-top: 10px">
+      <el-button type="danger" size="mini">批量删除</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,37 +36,41 @@ export default {
   name: 'PosMana',
   data () {
     return {
-      mystr: '',
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      input: '',
+      loading: true,
+      positions: [], // 存储从数据库中读取的position数组
+      pos: {}
     }
   },
+  mounted: function () {
+    this.loadPosData();
+  },
   methods: {
-    getString () {
-      this.$message({type: 'info', message: '我想从后端得到一段话？'});
+    loadPosData () {
       var _this = this;
-      this.getRequest('/system/basic/getString').then(resp => {
+      this.loading = true;
+      this.getRequest('/system/basic/pos/getAll').then(resp => {
         if (resp && resp.status === 200) {
-          // console.log(resp.data)
-          _this.mystr = resp.data.msg;
-          _this.$message({type: 'info', message: _this.mystr});
+          _this.positions = resp.data;
         }
-      })
+      });
+      _this.loading = false;
+    },
+    addPosition () {
+      var _this = this;
+      this.loading = true;
+      this.putRequest('/system/basic/pos/add', this.input).then(resp => {
+        if (resp && resp.status === 200) {
+          _this.loadPosData();
+        }
+      });
+      this.loading = false;
+    },
+    handleSelectionChange () {
+    },
+    handleEdit (index, row) {
+    },
+    handleDelete (index, row) {
     }
   }
 }
